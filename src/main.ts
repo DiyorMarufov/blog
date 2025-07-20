aeb6a976d310c1d064e7106e5e8675db630b2b06
DiyorMarufov <marufovdiyor9@gmail.com>
2025-07-20 16:20:05 +0500

    main.ts changed

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import config from './config';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = config.API_PORT;
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    }),
+  );
+  app.enableCors();
   await app.listen(PORT, () => console.log(`Server is running on port`, PORT));
 }
 bootstrap();
